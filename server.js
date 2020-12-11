0301b55331bdbc4dcc6eb1ea40ac54669fc48525
Will Kendall <will@wills-MacBook-Pro.local>
2020-12-11 01:56:33 -0800

    took out redundant handlebars

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
